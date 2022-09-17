97978621a37c415d8ecfbea18bfcf6e709edcd75
Yuta3577 <112237636+Yuta3577@users.noreply.github.com>
2022-09-17 20:53:38 +0800

    Summery

---
+++ Unity_HC_S_HunterShot_20220730/Assets/scripts/LearnField.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace YutaSpace
+{
+    /// <summary>
+    /// ¾Ç²ßÄæ¦ì Field
+    /// </summary>
+    public class LearnField : MonoBehaviour
+    {
+
+    }
+
+}
--- Unity_HC_S_HunterShot_20220730/Assets/scripts/firstscripts.cs
+++ Unity_HC_S_HunterShot_20220730/Assets/scripts/firstscripts.cs
@@ -6,7 +6,40 @@ using UnityEngine; // 引用Unity遊戲引擎命名空間：倉庫實用資料與功能
  * 多行註解
  * 多行註解
  */
-public class firstscripts : MonoBehaviour
+
+namespace YutaSpace
 {
-   
+    // Unity 類別要執行必須放在遊戲物件上
+    /// <summary>
+    /// 摘要：輔助說明並且會出現在編譯器提示上
+    /// first scripts 第一個腳本學習 C# 基礎與 Unity
+    /// </summary>
+    public class firstscripts : MonoBehaviour
+    {
+        #region 資料區域
+        //資料
+        //資料
+        #endregion
+
+        #region 事件區域：Unity 入口
+        /// <summary>
+        /// 喚醒事件：遊戲開始時並且在 start 前執行一次
+        /// </summary>
+        private void Awake()
+        {
+            print("哈囉，沃德:D");
+        }
+
+        /// <summary>
+        /// 開始事件：遊戲開始時並且在 Awake 後執行一次
+        /// </summary>
+        private void Start()
+        {
+            print("開始事件！");
+            // Rich Text
+            print("<color=yellow>黃色文字</color>");
+        }
+        #endregion
+    }
 }
+
