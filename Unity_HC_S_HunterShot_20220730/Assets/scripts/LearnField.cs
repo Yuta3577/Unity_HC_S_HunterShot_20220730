9e4ee7aed080f43aaa78e43845df3e35c2649d52
Yuta3577 <112237636+Yuta3577@users.noreply.github.com>
2022-09-17 21:05:26 +0800

    Summary2

--- Unity_HC_S_HunterShot_20220730/Assets/scripts/LearnField.cs
+++ Unity_HC_S_HunterShot_20220730/Assets/scripts/LearnField.cs
@@ -7,7 +7,13 @@ namespace YutaSpace
     /// </summary>
     public class LearnField : MonoBehaviour
     {
+        //欄位語法：
+        //修飾詞 資料類型 欄位名稱;
+        private int number;
 
+        //欄位語法：
+        //修飾詞 資料類型 欄位名稱 指定 值;
+        private int level = 1;
     }
 
 }
