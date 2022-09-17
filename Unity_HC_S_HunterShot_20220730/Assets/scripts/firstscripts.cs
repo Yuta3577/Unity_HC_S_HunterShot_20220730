26d145b76a63115fc83714144ead92aa41505112
Yuta3577 <112237636+Yuta3577@users.noreply.github.com>
2022-09-17 17:04:26 +0800

    Update firstscripts.cs

--- Unity_HC_S_HunterShot_20220730/Assets/scripts/firstscripts.cs
+++ Unity_HC_S_HunterShot_20220730/Assets/scripts/firstscripts.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// 單行註解
+using UnityEngine; // 引用Unity遊戲引擎命名空間：倉庫實用資料與功能
 
+// 腳本 = 類別
+/*
+ * 多行註解
+ * 多行註解
+ */
 public class firstscripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
